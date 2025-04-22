0da01c251b61a55a46313a102ac33f9ffdd77615
GiulianoArk <calcagnigiuliano770@gmail.com>
2025-04-22 17:44:04 -0300

    Agregue una card

--- tp-01/src/components/Main.jsx
+++ tp-01/src/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import '../css/main.css'
 const main = (props) => {
   return (
     <>
-    <div>
-        <h2>Hola soy {props.persona.nombre} {props.persona.apellido}</h2>
-        <p>Tengo {props.persona.edad} años</p>
-        <p>Mi email es {props.persona.email}</p>
-        <p>{props.persona.descripcion}</p>
+    <div className="card">
+        <h2>Nombre: {props.persona.nombre}</h2>
+        <h2>Apellido: {props.persona.apellido}</h2>
+        <h2>Correo: {props.persona.email}</h2>
+        <h3>{props.persona.descripcion}</h3>
+        <img src={props.image} alt={props.title} />
     </div>
     </>
   )
